d230d60ab1b6945123ed84bae7769d112d1cc705
Roberto Miura Honji <muirahonji@gmail.com>
2011-11-09 15:49:19 -0200

    Update replacement

--- run-config.py
+++ run-config.py
@@ -7,12 +7,29 @@ def main(home_dir):
 	for config in os.listdir(CONFIG_DIR):
 		src = os.path.abspath(os.sep.join([CONFIG_DIR, config]))
 		dst = os.sep.join([home_dir, '.%s' % config])
+		if os.path.islink(dst):
+			if os.readlink(dst) == src:
+				continue
+
+			msg = 'Link %s already exists to %s'
+			print msg % (dst, os.readlink(dst))
+			if raw_input('Would you like replace it? (Y/n)') in ['y', '']:
+				os.remove(dst)
+			else:
+				print 'ignoring file %s' % src
+				continue
+
 		if os.path.exists(dst):
-			print 'file %s exists' % dst
-			continue
+			os.system('diff -u %s %s | less' % (dst, src))
+			if raw_input('Substitute (Y/n) ').lower() in ['y', '']:
+				os.rename(dst, dst + '.bk')
+			else:
+				print 'ignoring file %s' % src
+				continue
 
 		os.symlink(src, dst)
 		print '$ ln -s %s %s' % (src, dst)
+	print 'done.'
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
